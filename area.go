5562c196d11ab84e9bb4f153f55303f81898a8b0
Isadora Faria <isadorabfaria12@gmail.com>
2022-10-21 16:50:05 -0300

    first commit

--- area.go
+++ area.go
@@ -11,7 +11,8 @@ func Circ(raio float64) float64 {
 	return math.Pow(raio, 2) * Pi
 }
 
-//Rect é responsável por calcular a área de um retângulo
+//Rect é responsável por calcular
+//a área de um retângulo
 func Rect(base, altura float64) float64 {
 	return base * altura
 }
